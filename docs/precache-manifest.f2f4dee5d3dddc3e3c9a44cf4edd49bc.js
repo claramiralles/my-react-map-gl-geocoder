bc77b1d9950c832356087a036d9881d5162034fb
Clara Miralles <clara.miralles.imperial@gmail.com>
2020-09-18 13:09:52 +0200

    npm run build + mv build docs to publish in github pages

---
+++ docs/precache-manifest.f2f4dee5d3dddc3e3c9a44cf4edd49bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb38218781ae1079b00893a98fdee0fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15c193f3a35585ec5ca2",
+    "url": "/static/css/2.1b78fa86.chunk.css"
+  },
+  {
+    "revision": "a7171e79f59b25f3c5e5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "15c193f3a35585ec5ca2",
+    "url": "/static/js/2.26f29449.chunk.js"
+  },
+  {
+    "revision": "b88bca802d90b4a3030e9a05cb0ffaf7",
+    "url": "/static/js/2.26f29449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7171e79f59b25f3c5e5",
+    "url": "/static/js/main.64fe929a.chunk.js"
+  },
+  {
+    "revision": "a3d2f138ca1bea0466a0",
+    "url": "/static/js/runtime-main.db736858.js"
+  }
+]);
